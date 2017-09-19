0fddcece6c8d18948257f75cf85f9e1397850d7e
Matěj <mckillem@icloud.com>
2017-09-19 11:42:37 +0200

    Splněno zadání

--- calculator.js
+++ calculator.js
@@ -1,6 +1,7 @@
 var operator = "";
 var number1 = "";
 var number2 = "";
+var result = 0;
 
 var calculations = {
     add: function add(a, b) {
@@ -21,122 +22,145 @@ var calculations = {
 }
 
 var numbers = {
-    one: function() {
+    displayTextContent: function displayTextContent(number) {
         var display = document.getElementById("display");
         if(display.textContent !== "0") {
-            display.textContent += "1";
+            display.textContent += number;
             return display.textContent;
         } else {
-            return display.textContent = "1";
+            return display.textContent = number;
         }
+    },
+    one: function() {
+        this.displayTextContent("1");
     },
     two: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "2";
-            return display.textContent;
-        } else {
-            return display.textContent = "2";
-        }
+        this.displayTextContent("2");        
     },
     three: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "3";
-            return display.textContent;
-        } else {
-            return display.textContent = "3";
-        }
+        this.displayTextContent("3"); 
     },
     four: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "4";
-            return display.textContent;
-        } else {
-            return display.textContent = "4";
-        }
+        this.displayTextContent("4"); 
     },
     five: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "5";
-            return display.textContent;
-        } else {
-            return display.textContent = "5";
-        }
+        this.displayTextContent("5"); 
     },
     six: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "6";
-            return display.textContent;
-        } else {
-            return display.textContent = "6";
-        }
+        this.displayTextContent("6"); 
     },
     seven: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "7";
-            return display.textContent;
-        } else {
-            return display.textContent = "7";
-        }
+        this.displayTextContent("7"); 
     },
     eight: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "8";
-            return display.textContent;
-        } else {
-            return display.textContent = "8";
-        }
+        this.displayTextContent("8"); 
     },
     nine: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "9";
-            return display.textContent;
-        } else {
-            return display.textContent = "9";
-        }
+        this.displayTextContent("9"); 
     },
     zero: function() {
-        var display = document.getElementById("display");
-        if(display.textContent !== "0") {
-            display.textContent += "0";
-            return display.textContent;
-        } else {
-            return display.textContent = "0";
-        }
+        this.displayTextContent("0"); 
     }
 }
 
 var handlers = {
-    addButton: function addButton() {
+    count: function count(operation) {
         var display = document.getElementById("display");
-        number1 = display.textContent;
-        operator = "+";
+        var calculation;
+
+        if(operator === "") {
+            switch (operation) {
+                case "add":
+                    calculation = calculations.add;
+                    operator = operation;
+                    break;
+                case "sub":
+                    calculation = calculations.sub;
+                    operator = operation;
+                    break;
+                case "multiply":
+                    calculation = calculations.multiply;
+                    operator = operation;                    
+                    break;
+                case "divide":
+                    calculation = calculations.divide;
+                    operator = operation;                    
+                    break;
+            }
+        } else {
+            if(operator === operation) {
+                switch (operation) {
+                    case "add":
+                        calculation = calculations.add;
+                        break;
+                    case "sub":
+                        calculation = calculations.sub;
+                        break;
+                    case "multiply":
+                        calculation = calculations.multiply;
+                        break;
+                    case "divide":
+                        calculation = calculations.divide;
+                        break;
+                }
+            } else {
+                switch (operator) {
+                    case "add":
+                        calculation = calculations.add;
+                        break;
+                    case "sub":
+                        calculation = calculations.sub;
+                        break;
+                    case "multiply":
+                        calculation = calculations.multiply;
+                        break;
+                    case "divide":
+                        calculation = calculations.divide;
+                        break;
+                }
+                operator = operation;
+            }
+            
+        }
+        
+
+        if(number1 === "") {
+            number1 = display.textContent;
+        } else {
+            number2 = display.textContent;
+            number1 = calculation(Number.parseFloat(number1), Number.parseFloat(number2));
+        }
+        
+        
+        console.log(number1);
         return display.textContent = "0";
+    },
+    addButton: function addButton() {
+        // var display = document.getElementById("display");
+        // var displayAll = document.getElementById("displayAll");
+        // if(number1 === "") {
+        //     number1 = display.textContent;
+        // } else {
+        //     number2 = display.textContent;
+        //     number1 = calculations.add(Number.parseFloat(number1), Number.parseFloat(number2));
+        // }
+        
+        
+        // console.log(number1);
+        // operator = "+";
+        // displayAll.textContent += display.textContent + operator;
+        // return display.textContent = "0";
+        this.count("add");
+        
     },
     subButton: function subButton() {
-        var display = document.getElementById("display");
-        number1 = display.textContent;
-        operator = "-";
-        return display.textContent = "0";
+        this.count("sub");
     },
     multiplyButton: function multiplyButton() {
-        var display = document.getElementById("display");
-        number1 = display.textContent;
-        operator = "*";
-        return display.textContent = "0";
+        this.count("multiply");
     },
     divideButton: function divideButton() {
-        var display = document.getElementById("display");
-        number1 = display.textContent;
-        operator = "/";
-        return display.textContent = "0";
+        this.count("divide");
     },
     clearButton: function clearButton() {
         var display = document.getElementById("display");
@@ -160,22 +184,21 @@ var handlers = {
         }
     },
     equalsButton: function equalsButton() {
-        var result = 0;
         var display = document.getElementById("display");
         number2 = display.textContent;
-        if(operator === "+") {
+        if(operator === "add") {
             result = calculations.add(Number.parseFloat(number1), Number.parseFloat(number2));
             return display.textContent = result; 
         }
-        if(operator === "-") {
+        if(operator === "sub") {
             result = calculations.sub(Number.parseFloat(number1), Number.parseFloat(number2));
             return display.textContent = result; 
         }
-        if(operator === "*") {
+        if(operator === "multiply") {
             result = calculations.multiply(Number.parseFloat(number1), Number.parseFloat(number2));
             return display.textContent = result; 
         }
-        if(operator === "/") {
+        if(operator === "divide") {
             result = calculations.divide(Number.parseFloat(number1), Number.parseFloat(number2));
             return display.textContent = result; 
         }      
